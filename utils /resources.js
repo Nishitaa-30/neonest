6cfc1765ce9870dd46fc05c413008eff3c158236
Nishitaa-30 <nishaaaaaa.0987@gmail.com>
2025-09-27 02:51:15 +0530

    Create resources.js

---
+++ utils /resources.js
@@ -0,0 +1,22 @@
+export const resources = {
+  babycare: [
+    {
+      title: "Caring for Your Baby: Birth to 1 Year",
+      type: "Article",
+      source: "Mayo Clinic",
+      url: "https://www.mayoclinic.org/healthy-lifestyle/infant-and-toddler-health"
+    }
+  ],
+  pregnancy: [
+    {
+      title: "Pregnancy Guide",
+      type: "Toolkit",
+      source: "WHO",
+      url: "https://www.who.int/health-topics/pregnancy"
+    }
+  ],
+  postpartum: [],
+  breastfeeding: [],
+  parenting: [],
+  others: []
+};
